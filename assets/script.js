d8102fdc8aa8ba88f6ba185549ac52adb842769b
Valérie Cote <libelul2a@gmail.com>
2025-01-14 23:16:41 +0100

    mise en place caroussel et event listeners des fleches

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,22 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-
-
-const arrowLeft = document.querySelector(".arrow_left");
-const arrowRight = document.querySelector(".arrow_right");
-
-// Ajout des Event Listeners
-arrowLeft.addEventListener("click", () => {
-    console.log("Flèche gauche cliquée !");
-});
-
-arrowRight.addEventListener("click", () => {
-    console.log("Flèche droite cliquée !");
-});
-
+//Generation des dots dynamiquement
 function createDots() {
     const dotsContainer = document.querySelector(".dots");// recuperation de la div dots
 	dotsContainer.innerHTML = "";//chaine vide pour reinitialiser le contenu de la balise html div dots
@@ -39,9 +26,46 @@ function createDots() {
         if (index === 0) {
             dot.classList.add("dot_selected"); // ajout de la class dot_selected au premier point (index 0)
         }
-        dotsContainer.appendChild(dot);//ajout de l'element dans la page avec appendchild, le rend visible
+        dotsContainer.appendChild(dot);//Ajoute dynamiquement chaque élément dot dans le conteneur de dots
     });
 }
-createDots();//fonction appelée
-  
+let currentIndex = 0; // Index de la slide active
+//Fonction de mise à jour du caroussel
+function updateSlide(index) {
+    const bannerImage = document.querySelector(".banner-img");
+    const bannerText = document.querySelector("#banner p");
+	const dots = document.querySelectorAll(".dot");
+
+    // Met à jour l'image et le texte
+	bannerImage.src = slides[index].image;
+    bannerText.innerHTML = slides[index].tagLine;
+	bannerImage.alt = slides[index].tagLine;
+
+// Met à jour les dots
+dots.forEach((dot, i) => {
+	dot.classList.toggle("dot_selected", i === index);
+});
+}
+// Ajout des Event Listeners
+const arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", () => {
+	/*if (currentIndex < slides.length - 1) {*/// avec cette condition avance seulement si on n'est pas au maximum
+        currentIndex++; //sans la condition if, affiche un message d'effeur (Uncaught TypeError: Cannot read properties of undefined (reading 'image'))
+        updateSlide(currentIndex);
+    }
+);
+
+const arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", () => {
+	/*if (currentIndex > 0) {*///  avec cette condition recule seulement si on n'est pas au minimum
+        currentIndex--; //sans la condition if, affiche un message d'effeur (Uncaught TypeError: Cannot read properties of undefined (reading 'image'))
+        updateSlide(currentIndex);
+    }
+);
+//Initialisation
+createDots();
+updateSlide(currentIndex);
+
+
+
 
